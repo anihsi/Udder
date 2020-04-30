f4d17898237675abeaedfd787b6501cdb6a4c642
Ekaterina Ishina <anihsi@yahoo.com>
2020-04-30 21:35:24 +0400

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Екатерина Ишина on 4/30/20.
+//  Copyright © 2020 Екатерина Ишина. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
